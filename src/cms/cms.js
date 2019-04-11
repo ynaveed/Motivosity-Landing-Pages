a3c04857146c2ccfba408d463c5492d08290913a
yasir.naveed <yasir.naveed@oetkerdigital.com>
2019-04-11 17:32:22 +0200

    Add preview for cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,13 @@
 import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
+import AboutUsPreview from './preview-templates/AboutUsPreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('about Us', AboutUsPreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
---
+++ src/cms/preview-templates/AboutUsPreview.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { AboutUsTemplate } from '../../templates/about-us'
+
+const AboutUsPreview = ({ entry, widgetFor }) => (
+  <AboutUsTemplate
+    title={entry.getIn(['data', 'title'])}
+    content={widgetFor('body')}
+  />
+)
+
+AboutUsPreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
+
+export default AboutUsPreview

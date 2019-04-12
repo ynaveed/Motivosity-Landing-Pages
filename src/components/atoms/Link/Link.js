39947b6f0bd5696642d12fd4609198c19d99c6d1
yasir.naveed <yasir.naveed@oetkerdigital.com>
2019-04-12 22:59:49 +0200

    Add components

---
+++ src/components/atoms/Link/Link.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled, { css } from 'styled-components/macro';
+import { Link as Anchor } from "gatsby";
+
+const Link = styled(Anchor)`
+
+`;
+
+export default Link
+
